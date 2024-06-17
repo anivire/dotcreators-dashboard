e6366c93e7a28f2b5a2cee54efe3a15314530223
Nikita Grichan <anivire.work@gmail.com>
2024-06-17 05:29:53 +0300

    fix: added top margin if banner is not provided

--- src/components/ArtistsSearchComponents/ArtistCardProfile.tsx
+++ src/components/ArtistsSearchComponents/ArtistCardProfile.tsx
@@ -208,10 +208,16 @@ export const ArtistCardProfile: FC<Props> = props => {
                     </div>
                   </>
                 )}
+
                 <div
                   className={classNames('flex w-full flex-col gap-5 px-5 pb-5')}
                 >
-                  <div className="z-20 flex flex-row items-center justify-between gap-3">
+                  <div
+                    className={classNames(
+                      'z-20 flex flex-row items-center justify-between gap-3',
+                      { 'mt-5': !props.artist.images.banner }
+                    )}
+                  >
                     <div className="z-20 flex w-full flex-row items-center justify-between gap-3">
                       <div className="flex flex-row items-center gap-3">
                         <ImageLoader
