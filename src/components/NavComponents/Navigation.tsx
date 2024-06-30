2a0867c6960285ae059bcdeda728ed6f6051c170
Nikita Grichan <anivire.work@gmail.com>
2024-06-30 20:26:30 +0300

    feat: load user data

--- src/components/NavComponents/Navigation.tsx
+++ src/components/NavComponents/Navigation.tsx
@@ -7,9 +7,47 @@ import RiBrushFill from '~icons/ri/brush-fill';
 import RiSettings4Fill from '~icons/ri/settings-4-fill';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
+import useSWR from 'swr';
 
-export function Navigation() {
+export default function Navigation() {
   const router = useRouter();
+
+  // const data = await fetch(`${process.env.API_URL}auth/user`).then(
+  //   async response => {
+  //     if (response.ok) {
+  //       const data: {
+  //         status: string;
+  //         response: {
+  //           id: string;
+  //           githubId: string;
+  //           username: string;
+  //           avatarUrl: string;
+  //         };
+  //       } = await response.json();
+
+  //       return data.response;
+  //     } else {
+  //       throw new Error('Failed to fetch artist profiles');
+  //     }
+  //   }
+  // );
+
+  const { data: user, error } = useSWR<{
+    status: string;
+    response: {
+      id: string;
+      githubId: string;
+      username: string;
+      avatarUrl: string;
+    };
+  }>(
+    `${process.env.API_URL}auth/user`,
+    async (input: RequestInfo, init: RequestInit) => {
+      const res = await fetch(input, { ...init, credentials: 'include' });
+      return res.json();
+    },
+    {}
+  );
 
   return (
     <section className="flex h-screen max-w-sm flex-col gap-16 border-r border-dot-white/5 bg-dot-body p-8">
@@ -17,30 +55,32 @@ export function Navigation() {
         <DotcreatorsLogoResponsive width={30} height={28} color="#FF902B" />
       </div>
 
-      <div className="flex flex-row items-center justify-between gap-3 overflow-hidden rounded-full bg-dot-primary p-3 px-5">
-        <div className="relative flex flex-row items-center gap-3">
-          <ImageLoader
-            src="https://cdn.discordapp.com/avatars/207839349209956352/b68c555ffa7699573252c2dab6b75887.png"
-            alt="Avatar for anivire"
-            width={35}
-            height={35}
-            className="rounded-full"
-          />
-          <div className="absolute z-10 ">
+      {user && (
+        <div className="flex flex-row items-center justify-between gap-3 overflow-hidden rounded-full bg-dot-primary p-3 px-5">
+          <div className="relative flex flex-row items-center gap-3">
             <ImageLoader
-              src="https://cdn.discordapp.com/avatars/207839349209956352/b68c555ffa7699573252c2dab6b75887.png"
+              src={user.response.avatarUrl}
               alt="Avatar for anivire"
               width={35}
               height={35}
-              className="rounded-full opacity-50 blur-xl"
+              className="rounded-full"
             />
+            <div className="absolute z-10 ">
+              <ImageLoader
+                src={user.response.avatarUrl}
+                alt="Avatar for anivire"
+                width={35}
+                height={35}
+                className="rounded-full opacity-50 blur-xl"
+              />
+            </div>
+            <p>{user.response.username}</p>
           </div>
-          <p>änivire</p>
+          <button className="text-zinc-400">
+            <RiSettings4Fill />
+          </button>
         </div>
-        <button className="text-zinc-400">
-          <RiSettings4Fill />
-        </button>
-      </div>
+      )}
 
       <div className="flex w-full flex-col gap-1.5">
         <Link
